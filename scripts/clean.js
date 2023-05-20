a013c580d3a713db399e17124e318900189ce915
Ha Do <hado@intelisysaviation.com>
2023-05-20 22:53:27 +0700

    Rename folder dist to docs

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,7 +1,8 @@
 const sh = require('shelljs');
 const upath = require('upath');
 
-const destPath = upath.resolve(upath.dirname(__filename), '../dist');
+// const destPath = upath.resolve(upath.dirname(__filename), '../dist');
+const destPath = upath.resolve(upath.dirname(__filename), '../docs');
 
 sh.rm('-rf', `${destPath}/*`)
 
